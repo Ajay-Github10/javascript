21d17ee14f900f4379c207c5b8247dc55efb55e6
Ajay Zalte <ajayzofficial01@gmail.com>
2024-06-27 01:08:17 +0530

    vaiables

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 988774455
+let accountEmail = "ajay@gmail.com"
+var accountPass = "Ajay@123"
+accountCity = "Nashik"
+
+// accountId = 22
+let accountState;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
+
+/*
+Prefer to do not use var because of issue in block and functional scope
+*/

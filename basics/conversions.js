3033a1851aef8307efb01045f12dd915dea14b87
Ajay Zalte <ajayzofficial01@gmail.com>
2024-06-27 01:49:55 +0530

    conversions of dataTypes

---
+++ basics/conversions.js
@@ -0,0 +1,27 @@
+let score =  "AJAY ZALTE"
+ 
+// console.log(typeof(score));
+
+// let valueNumber = Number(score);
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "12354" => 12345
+//"55abc" => NaN  only type is number bcoz of conversion
+
+// true => 1 and false => 0   boolean values.
+
+let isLoggedIn = "AJAY"
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(isLoggedIn));
+
+// 1 => true;  0 => false
+
+let someNumber = 55
+let stringNumber = String(someNumber);
+
+console.log(typeof someNumber);
+console.log(typeof stringNumber);

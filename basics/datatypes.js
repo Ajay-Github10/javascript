1449d5349fea04b18ed62d0ab13e689c5dbbc3f4
Ajay Zalte <ajayzofficial01@gmail.com>
2024-06-27 01:12:31 +0530

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";     //treat all js  code as newer version.
+// alert(3*3) not in node js go to browser.
+
+console.log(10*30);
+console.log('AJAY');
+
+
+let name = "ajay"
+let number = 9145135739
+let city = "Nashik"
+let age = 25
+
+// number => 2 to power 53
+// Bigint
+// boolean => true/false
+// string => "ajay zalte nashik"
+// null => standalone value  
+// undefined
+//symbol => unique
+
+//object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

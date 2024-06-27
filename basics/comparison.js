65ecc0712b8476148f59db7d3a72804ba7788b75
Ajay Zalte <ajayzofficial01@gmail.com>
2024-06-28 01:29:18 +0530

    Comparisons operators

---
+++ basics/comparison.js
@@ -0,0 +1,24 @@
+// console.log(2<1);
+// console.log(2 == 2);
+// console.log(5 > 2);
+// console.log(5 >= 4);
+// console.log(10 <= 4);
+// console.log(5 != 5);
+
+// console.log(null > 0);
+// console.log( null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);    // avoid such comparisons
+
+// == (double check)
+// console.log("2" == 2);    //true  conversions happens 
+
+//===
+// console.log("5" === 5);    // false === checked value + dataTypes
+
+
+
